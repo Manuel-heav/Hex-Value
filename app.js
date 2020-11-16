a3c7ac307699a6a0594803ee307a51bbc7e4517a
Manuel-heav <Manuelheav32@gmail.com>
2020-11-17 01:47:13 +0700

    progress

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // Declaring Variables
 const btn = document.querySelector('.button');
 const hexValue = document.querySelector('.hexValue')
-const hexArray = ['A', 'B', 'C', 'D', 'E','F', 1 , 2 , 3 , 4, 5, 6, 7 ,8 , 9];
+const hexArray = ['A', 'B', 'C', 'D', 'E','F', 1 , 2 , 3 , 4, 5, 6, 7 ,8 , 9, 0];
 const body = document.querySelector('body'); 
 // Event Listener
-btn.addEventListener('click', (e)=>{
+btn.addEventListener('click', changeColor());
+
+function changeColor(){
+	let h = '#';
 	for(i = 0; i < 6; i++){
-		const value = '#'+Math.floor(Math.random()*hexArray.length)
+		const value = Math.floor(Math.random()*hexArray.length);
+		h += hexArray[value];
 	}
-	body.style.backgroundColor = value;
-	hexValue.innerHTML = value;
-});
+	body.style.backgroundColor = h;
+	hexValue.innerHTML = h;
+}
 
