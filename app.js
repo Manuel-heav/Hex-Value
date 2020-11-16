9701f4622d2cb1d76f6d2ef54f5f4d28e1b9ebfd
Manuel-heav <Manuelheav32@gmail.com>
2020-11-17 01:50:29 +0700

    Finally

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hexValue = document.querySelector('.hexValue')
 const hexArray = ['A', 'B', 'C', 'D', 'E','F', 1 , 2 , 3 , 4, 5, 6, 7 ,8 , 9, 0];
 const body = document.querySelector('body'); 
 // Event Listener
-btn.addEventListener('click', changeColor());
+btn.addEventListener('click', changeColor);
 
 function changeColor(){
 	let h = '#';
