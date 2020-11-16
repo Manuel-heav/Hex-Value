f4c578b7245dbdfe9ef91abf38f8e7f5dc2bf807
Manuel-heav <Manuelheav32@gmail.com>
2020-11-17 01:18:29 +0700

    added a background value

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 const btn = document.querySelector('.button');
 const hexValue = document.querySelector('.hexValue')
 const hexArray = ['A', 'B', 'C', 'D', 'E','F', 1 , 2 , 3 , 4, 5, 6, 7 ,8 , 9];
-
+const body = document.querySelector('body'); 
 // Event Listener
 btn.addEventListener('click', (e)=>{
 	for(i = 0; i < 6; i++){
 		const value = '#'+Math.floor(Math.random()*hexArray.length)
 	}
-	hexValue.style.backgroundColor = value;
+	body.style.backgroundColor = value;
 	hexValue.innerHTML = value;
 });
 
